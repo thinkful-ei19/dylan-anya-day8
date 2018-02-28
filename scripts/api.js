0f666da001529997fe9c69c213bf364b433ab216
Dylan Stratton <dm.stratton94@gmail.com>
2018-02-28 10:40:49 -0600

    add createItems method on api for ajax POST request

--- scripts/api.js
+++ scripts/api.js
@@ -9,5 +9,22 @@ const api = (function() {
 
     callback('api module works!');
   };
-  return {getItems};
+
+  const createItem = function(name, callback) {
+    const newItem = JSON.stringify({name});
+
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+
+  };
+
+  return {
+    getItems,
+    createItem
+  };
 }());
