67a09cd7dbdf0fea873b184e00701fc09bace790
Dylan Stratton <dm.stratton94@gmail.com>
2018-02-28 10:31:13 -0600

    getJSON test with getItems()

--- scripts/api.js
+++ scripts/api.js
@@ -1,16 +1,13 @@
-/* global api */
+/* global api, $ */
 'use strict';
 
 const api = (function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/dylan-anya';
   const getItems = function(callback) {
+
+    $.getJSON(`${BASE_URL}/items`, callback);
+
     callback('api module works!');
   };
   return {getItems};
 }());
-
-api.getItems(function(data) {
-  console.log(data);
-});
-
-console.log(api.BASE_URL);
